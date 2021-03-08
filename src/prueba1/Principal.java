1d29491e619d895a42d2862e9090defc57b72803
rubenescudero01 <rubenescudero01@gmail.com>
2021-03-08 20:39:10 +0100

    Validamos nuevos cambios en la clase principal 2º commit

--- src/prueba1/Principal.java
+++ src/prueba1/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Línea de texto 1");
+		System.out.println("Línea de texto 2");
 
 	}
 
