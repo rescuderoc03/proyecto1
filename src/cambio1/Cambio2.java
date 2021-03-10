c5f15165eae2baf32fcab16dbffdb6ec4f896b29
rubenescudero01 <rubenescudero01@gmail.com>
2021-03-10 19:32:18 +0100

    Cambio2. Commit 6

---
+++ src/cambio1/Cambio2.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Clase Cambio2 en proyecto1");
+
+	}
+
+}
